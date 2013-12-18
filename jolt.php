4c84b9ab4f3c8b7f0ec2ced9a291576d7a1cdd57
Roger Stringer <roger@freekrai.net>
2013-12-18 13:04:56 -0800

    added cli class

--- jolt.php
+++ jolt.php
@@ -599,6 +599,26 @@ abstract class Jolt_Controller{
 	}
 
 }
+/*
+	Command-line utility class for when you run apps via command-line.
+*/
+abstract class Jolt_CLI{
+	public arguments = array();
+	public function __construct( $argv ){
+		$this->arguments = $this->get_arguments( $argv );
+	}
+	private function get_arguments($argv) {
+		$_ARG = array();
+		foreach ($argv as $arg) {
+			if ( preg_match('/--(.*)=(.*)/',$arg,$reg) ) {
+				$_ARG[$reg[1]] = $reg[2];
+			} elseif( preg_match('/-([a-zA-Z0-9])/',$arg,$reg) ) {
+				$_ARG[$reg[1]] = 'true';
+			}
+		}
+		return $_ARG;
+    }
+}
 
 class Jolt_Set implements \ArrayAccess, \Countable, \IteratorAggregate{
 	protected $data = array();
